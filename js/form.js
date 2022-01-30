3cdbc0e22deb54fe17be727cf1a29a229407d5d8
ederteixeira <ccbeder@gmail.com>
2022-01-30 15:26:48 -0300

    add funcao

--- js/form.js
+++ js/form.js
@@ -2,26 +2,38 @@
 var botaoAdd = document.querySelector("#adicionar-paciente");
 botaoAdd.addEventListener("click", function(event){
     event.preventDefault();
-    
-    var form = document.querySelector("#form-add");
-    
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
 
+    var form = document.querySelector("#form-add");
+    var paciente = obtemPaciente(form);
+    var pacientetr = montaTr(paciente);
+
+});
+
+function obtemPaciente(form){
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+    return paciente;
+}
+
+function montaTr(paciente){
     var pacienteTr = document.createElement("tr");
+
     var nomeTd = document.createElement("td");
     var pesoTd = document.createElement("td");
     var alturaTd = document.createElement("td");
     var gorduraTd = document.createElement("td");
     var imcTd = document.createElement("td");
 
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaImc(peso, altura);
+    nomeTd.textContent = paciente.nome;
+    pesoTd.textContent = paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.textContent = paciente.imc;
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
@@ -32,4 +44,5 @@ botaoAdd.addEventListener("click", function(event){
     var tabela = document.querySelector("#tabela-pacientes");
     tabela.appendChild(pacienteTr);
 
-});
+    return pacienteTr;
+}
